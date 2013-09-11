523223c8d830a9410a8d2613f1567467a61c781a
Wanderson Paim de Jesus <wanderson.paim@gmail.com>
2013-09-10 22:52:57 -0300

    Arquivos para delete do vip

--- provinet/infrastructure/setup/views.py
+++ provinet/infrastructure/setup/views.py
@@ -60,26 +60,6 @@ def newVIP (request):
         form = NewVIPForm()
         return render_to_response('infrastructure/setup/new_vip.html', RequestContext(request, {'form': form, }))
 
-class VIPUpdate(UpdateView):
-    model = VIP
-    fields = ['name','address','uri','location','protocol','method','is_active']
-    template_name_suffix = '_update_form'
-
-def updateVIP (request):
-    """
-    Update a VIP
-    """
-    
-    if request.method == 'POST':
-        form = VIPUpdate(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "VIP successfully created!")
-            return HttpResponseRedirect('/setup/vips')
-    else:
-        return render_to_response('infrastructure/setup/new_vip.html', RequestContext(request, {'form': form, }))    
-
-
 def deleteResourcePool (request, resourcepool_id):
     if request.method == 'GET':
         rp = ResourcePool.objects.filter(id=resourcepool_id)
@@ -102,3 +82,24 @@ def deleteVIP (request, vip_id):
             messages.success(request, "VIP not found!")
             return HttpResponseRedirect('/setup/vips')
 
+def updateResourcePool (request, resourcepool_id):
+    if request.method == 'GET':
+        rp = ResourcePool.objects.filter(id=resourcepool_id)
+        if rp.exists():
+            rp.delete()
+            messages.success(request, "Resource Pool successfully deleted!")
+            return HttpResponseRedirect('/setup/resource_pools')
+        else:
+            messages.success(request, "Resource Pool not found!")
+            return HttpResponseRedirect('/setup/resource_pools')
+
+def updateVIP (request, vip_id):
+    if request.method == 'GET':
+        rp = VIP.objects.filter(id=vip_id)
+        if rp.exists():
+            rp.delete()
+            messages.success(request, "VIP successfully deleted!")
+            return HttpResponseRedirect('/setup/vips')
+        else:
+            messages.success(request, "VIP not found!")
+            return HttpResponseRedirect('/setup/vips')
