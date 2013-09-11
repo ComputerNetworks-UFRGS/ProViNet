7ad266096817ca6d757179bdc80219427b56ce57
Wanderson Paim de Jesus <wanderson.paim@gmail.com>
2013-09-11 00:00:23 -0300

    Arquivos para delete do vip

--- provinet/infrastructure/setup/models.py
+++ provinet/infrastructure/setup/models.py
@@ -28,7 +28,7 @@ class VIP (models.Model):
     address = models.CharField(max_length=50, null=False)
     uri = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=700)
-    prototol = models.CharField(max_length=5,choices=PROTOCOLS, null=False)
+    protocol = models.CharField(max_length=5,choices=PROTOCOLS, null=False)
     method = models.CharField(max_length=5,choices=METHODS, null=False)
     is_active = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
